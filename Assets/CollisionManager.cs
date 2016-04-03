2c0623785efeb645388534c12fb111279c0c0700
Fleen Mobile <FleenMobile@gmail.com>
2016-04-03 19:15:09 +0200

    Add bot respawns
    After each collision bot moves randomly in x,z directions. This makes it look like he's respawning in random place ;)

--- Assets/CollisionManager.cs
+++ Assets/CollisionManager.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 
 public class CollisionManager : MonoBehaviour {
+	private Vector3 m_Move;
 
 	void OnCollisionEnter (Collision col)
 	{
 		if(col.collider.tag == "Bot1")
 		{
 			ScoreManager.score += 10;
-			Destroy(col.gameObject);
+
+			// Move the bot randomly
+			System.Random r = new System.Random ();
+			m_Move = new Vector3 (r.Next(5, 15) * 1.0f, 0.0f, r.Next(5, 15) * 1.0f);
+			col.gameObject.transform.position += m_Move;
 		}
 	}
 	
