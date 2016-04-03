2279623febed795aaeb88d3ac80bc0c556b39420
Bartosz Rakoczy <bartosz.rakoczy@gmail.com>
2016-04-03 17:14:38 +0200

    Scoring system added. Collisions with bots implemented.

---
+++ Assets/CollisionManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionManager : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.collider.tag == "Bot1")
+		{
+			ScoreManager.score += 10;
+			Destroy(col.gameObject);
+		}
+	}
+	
+}
