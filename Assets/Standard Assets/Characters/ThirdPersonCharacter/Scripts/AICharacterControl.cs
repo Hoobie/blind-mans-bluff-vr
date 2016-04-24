5553f910bfee2092077737104e1e56d942cef8cb
Hoobie <hoobie92@gmail.com>
2016-04-24 15:21:45 +0200

    Add new AI POC

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -3,64 +3,77 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (NavMeshAgent))]
-    [RequireComponent(typeof (ThirdPersonCharacter))]
-    public class AICharacterControl : MonoBehaviour
-    {
-        public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
-        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform target;     
-		public System.Random r;
+	[RequireComponent (typeof(NavMeshAgent))]
+	[RequireComponent (typeof(ThirdPersonCharacter))]
+	public class AICharacterControl : MonoBehaviour
+	{
+		public NavMeshAgent agent { get; private set; }
+		// the navmesh agent required for the path finding
+		public ThirdPersonCharacter character { get; private set; }
+		// the character we are controlling
 		public Transform player;
-		public Transform myTransform;
+		public Transform target;
+		public bool hiding = false;
 
-        private void Start()
-        {
-            // get the components on the object we need ( should not be null due to require component so no need to check )
-            agent = GetComponentInChildren<NavMeshAgent>();
-			character = GetComponent<ThirdPersonCharacter>();
-			myTransform = character.transform;
+		private void Start ()
+		{
+			// get the components on the object we need ( should not be null due to require component so no need to check )
+			agent = GetComponentInChildren<NavMeshAgent> ();
+			character = GetComponent<ThirdPersonCharacter> ();
 
-	        agent.updateRotation = false;
-	        agent.updatePosition = true;
-
-			r = new System.Random (Guid.NewGuid().GetHashCode());
-			InvokeRepeating ("ChangeTarget", 0.0f, 1f);
+			agent.updateRotation = false;
+			agent.updatePosition = true;
 
 			player = GameObject.FindWithTag ("Player").transform;
-        }
-
-		private void ChangeTarget() {
-			double nextDouble = r.NextDouble ();
-
-			if (nextDouble < 0.25) 
-				this.target = GameObject.Find ("Tree").transform;
-			else if (nextDouble < 0.5)
-				this.target = GameObject.Find ("Tree (1)").transform;
-			else if (nextDouble < 0.75)
-				this.target = GameObject.Find ("Tree (2)").transform;
-			else
-				this.target = GameObject.Find ("Tree (3)").transform;
 		}
 
+		private void Update ()
+		{
+			character.transform.LookAt (player);
 
-        private void Update()
-        {
-            if (target != null)
-                agent.SetDestination(target.position);
+			if (!IsPlayerLooking () && !hiding) {
+				hiding = true;
+				FindClosestWall ();
+				agent.SetDestination (target.position);
+				if (agent.remainingDistance > agent.stoppingDistance) {
+					character.Move (agent.desiredVelocity, false, false);
+				}
+			} else {
+				hiding = false;
+				MoveFromPlayer ();
+			}
+		}
 
-            if (agent.remainingDistance > agent.stoppingDistance)
-                character.Move(agent.desiredVelocity, false, false);
-            else
-                character.Move(Vector3.zero, false, false);
+		private bool IsPlayerLooking ()
+		{
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+			return Physics.Raycast (ray, out hit, 100) && hit.transform.tag == "Player";
+		}
 
-			myTransform.LookAt (player);
-        }
+		private void FindClosestWall ()
+		{
+			GameObject[] gos;
+			gos = GameObject.FindGameObjectsWithTag ("Wall");
+			GameObject closest = null;
+			float distance = Mathf.Infinity;
+			Vector3 position = transform.position;
+			foreach (GameObject go in gos) {
+				Vector3 diff = go.transform.position - position;
+				float curDistance = diff.sqrMagnitude;
+				if (curDistance < distance) {
+					closest = go;
+					distance = curDistance;
+				}
+			}
+			target = closest.transform;
+		}
 
-
-        public void SetTarget(Transform target)
-        {
-            this.target = target;
-        }
-    }
+		private void MoveFromPlayer ()
+		{
+			Vector3 moveDirection = character.transform.position - player.position;
+			float speed = 0.2f;
+			character.Move (moveDirection.normalized * speed, false, false);
+		}
+	}
 }
