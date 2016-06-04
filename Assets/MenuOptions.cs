f69e5c527b5d9e71fb5decf3f3d5d9e275c648b9
Fleen Mobile <FleenMobile@gmail.com>
2016-06-04 22:16:25 +0200

    Assign 'Start game' and 'Quit' buttons
    First one start Main scene and second one quits the app

---
+++ Assets/MenuOptions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame() {
+		Application.LoadLevel ("Main");
+	}
+
+	public void ShowHighscores() {
+	}
+
+	public void QuitApp() {
+		Application.Quit();
+	}
+}
