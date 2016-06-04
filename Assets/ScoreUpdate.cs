899efb16626aaef0d4ca7fad7e0e8e1a0b3a1adf
Fleen Mobile <FleenMobile@gmail.com>
2016-06-04 22:55:50 +0200

    Add finish menu
    Displayed after finishing the game. With displaying score and retry button.
    Score is being taken from a special class which contains parameters pushed between scenes.

---
+++ Assets/ScoreUpdate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class ScoreUpdate : MonoBehaviour
+{
+	void Start() {
+		string score = Scenes.getParam ("score");
+		GameObject scoreText = GameObject.Find ("Score");
+		scoreText.GetComponent<UnityEngine.UI.Text> ().text += score;
+	}
+}
