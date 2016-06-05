43346a9e0186d8160e01c1743c651f01e24800cd
Fleen Mobile <FleenMobile@gmail.com>
2016-06-05 11:09:21 +0200

    Add PauseMenu on NFC click
    Whenever the user presses magnet button while in game, it shows Pause Menu with his score. He can than resume the game (however I'm not passing time yet, so user can play indefinately but I'll change it xD)

---
+++ Assets/CardboardTriggerListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class CardboardTriggerListener : MonoBehaviour
+{
+	void OnEnable() {
+		Cardboard.SDK.OnTrigger += TriggerPulled;	
+	}
+
+	void OnDisable() {
+		Cardboard.SDK.OnTrigger -= TriggerPulled;
+	}
+
+	void TriggerPulled() {
+		Scenes.Load ("PauseMenu", "Score", ScoreManager.score.ToString ());
+	}
+}
+
